78ba8ed1227527e11e6c18f297038c4781ccb398
Zohar <zohar.s0s@gmail.com>
2024-05-05 10:23:35 +0300

    ,

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
-import "./App.css";
+import { LoginPage } from "./components/pages/LoginPage";
+
 
 function App() {
-  return <div className="App">hello world</div>;
+  return (
+    <>
+        <LoginPage />
+    </>
+  );
 }
 
 export default App;
