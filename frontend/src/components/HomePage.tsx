665befd811955779a898d595ba13e7b70757b4d0
Zohar <zohar.s0s@gmail.com>
2024-05-18 07:09:51 +0300

    updates

--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -1,18 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { VacationCard } from "./Vacations/VacationCard";
+import { Navigate } from "react-router-dom";
 import { Box, Typography, styled } from "@mui/material";
 import { getAllVacationsService } from "../services/vacationServices/getVacations";
 import { VacationType } from "../types/VacationType";
 import { useAppSelector } from "../App/hooks";
 import { selectUser } from "../App/features/usersSlice";
-import { ErrorMessage } from "./ErrorMessage";
-import { Navigate, useNavigate } from "react-router-dom";
+import { VacationCard } from "./Vacations/VacationCard";
+import FilterAltIcon from "@mui/icons-material/FilterAlt";
 
 const HomeBox = styled(Box)({
   height: "100vh",
   display: "flex",
   flexDirection: "column",
-  gap: "50px",
+  // gap: "50px",
   padding: "150px",
 });
 
@@ -24,6 +24,14 @@ const CardsBox = styled(Box)({
   boxSizing: "border-box",
   gap: "10px",
   justifyContent: "center",
+});
+
+const FilterBox = styled(Box)({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  padding: "30px",
+  boxSizing: "border-box",
 });
 
 export const HomePage = () => {
@@ -41,28 +49,40 @@ export const HomePage = () => {
     };
 
     getAllVacations();
-  }, [user.token, vacations]);
+  }, [user.token]);
 
   return (
     <HomeBox>
+      <FilterBox>
+        <Typography
+          sx={{ textAlign: "center", color: "white", fontFamily: "tripSans" }}
+        >
+          <FilterAltIcon
+            sx={{ fontSize: "20px", color: "white", marginRight: "10px" }}
+          />
+          Filter Vacations
+        </Typography>
+      </FilterBox>
       <CardsBox>
-        {user.token
-          ? vacations.map((v, i) => (
-              <VacationCard
-                key={i}
-                _id={v._id}
-                locationCountry={v.locationCountry}
-                locationCity={v.locationCity}
-                description={v.description}
-                startDate={v.startDate}
-                endDate={v.endDate}
-                price={v.price}
-                imageName={v.imageName}
-                usersFollowed={v.usersFollowed}
-              />
-            ))
-          : <Navigate to ="/" />}
+        {user.token ? (
+          vacations.map((v, i) => (
+            <VacationCard
+              key={i}
+              _id={v._id}
+              locationCountry={v.locationCountry}
+              locationCity={v.locationCity}
+              description={v.description}
+              startDate={v.startDate}
+              endDate={v.endDate}
+              price={v.price}
+              imageName={v.imageName}
+              usersFollowed={v.usersFollowed}
+            />
+          ))
+        ) : (
+          <Navigate to="/" />
+        )}
       </CardsBox>
     </HomeBox>
-  )
+  );
 };
